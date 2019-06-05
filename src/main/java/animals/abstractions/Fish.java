2213b17bd057fc510005a360f35c57a5790f4bf3
Seweryn91 <s.talaj@gmail.com>
2019-06-05 17:47:54 +0200

    Inherit methods from class Animal

--- src/main/java/animals/abstractions/Fish.java
+++ src/main/java/animals/abstractions/Fish.java
@@ -2,7 +2,7 @@ package animals.abstractions;
 
 import animals.interfaces.Swim;
 
-public abstract class Fish implements Swim {
+public abstract class Fish extends Animal implements Swim {
 
     public void dive() {
         System.out.println(getClass().getSimpleName() + " can dive");
