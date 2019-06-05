8a3e150397d70a3b08e67cb2075bf53975fa35a4
Seweryn91 <s.talaj@gmail.com>
2019-06-05 17:48:20 +0200

    Implement method makeSound inherited from superclass Animal

--- src/main/java/animals/Salmon.java
+++ src/main/java/animals/Salmon.java
@@ -9,4 +9,14 @@ public class Salmon extends Fish implements Swim {
     public void swim() {
         System.out.println(getClass().getSimpleName() + " can swim in salt and fresh water");
     }
+
+    @Override
+    public void eat() {
+        System.out.println(getClass().getSimpleName() + " eats smaller fish as well as other animals");
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Some fish can make sound that is not audible to human ears");
+    }
 }
