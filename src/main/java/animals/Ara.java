e88a5fc2fce5ec97f042005a6008a903aa6203bb
Seweryn91 <s.talaj@gmail.com>
2019-06-05 16:29:50 +0200

    Create class Ara

---
+++ src/main/java/animals/Ara.java
@@ -0,0 +1,27 @@
+package animals;
+
+import animals.abstractions.Bird;
+import animals.interfaces.Fly;
+
+public class Ara extends Bird implements Fly
+{
+    @Override
+    void peck() {
+        System.out.println(getClass().getSimpleName() + " can peck painfully");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getClass().getSimpleName() + " eats different parts of plants");
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(getClass().getSimpleName() + " is very loud");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getClass().getSimpleName() + " is excellent skydiver");
+    }
+}
